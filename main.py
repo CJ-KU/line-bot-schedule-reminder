218de213864eedfed9aba70ae9fc3abc683aef6a
CJ-KU <jerry85606@gmail.com>
2025-05-05 18:14:49 +0800

    Update main.py

--- main.py
+++ main.py
@@ -81,12 +81,15 @@ def reverse_geocode_town(lat, lng):
         data = response.json()
         if data["status"] == "OK":
             level3 = None
+            level2 = None
             for comp in data["results"][0]["address_components"]:
                 if "administrative_area_level_3" in comp["types"]:
                     level3 = comp["long_name"]
-                    break  # 找到區、鄉、鎮就停
-            print(f"🏞️ 取得行政區：{level3}")
-            return level3
+                elif "administrative_area_level_2" in comp["types"]:
+                    level2 = comp["long_name"]
+            town_name = level3 or level2
+            print(f"🏞️ 取得行政區（fallback）：{town_name}")
+            return town_name
         print("⚠️ Reverse geocode 找不到行政區")
         return None
     except Exception as e:
